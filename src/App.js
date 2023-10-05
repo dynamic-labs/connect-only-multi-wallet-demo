7bc9f611f8597363ebdd52f3220a8647525a3124
Ryan Gilbert <ryangilbert7926@gmail.com>
2023-10-05 12:43:25 -0400

    feat: add demo for multi wallet connect only

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { getEthersSigner } from "@dynamic-labs/ethers-v5";
+import {
+  DynamicContextProvider,
+  useDynamicContext,
+} from "@dynamic-labs/sdk-react";
 
-function App() {
+function Page() {
+  const {
+    setShowAuthFlow,
+    setPrimaryWallet,
+    primaryWallet,
+    secondaryWallets,
+    handleUnlinkWallet,
+  } = useDynamicContext();
+
+  const [signature, setSignature] = useState('')
+
+  async function signMessage() {
+    if (!primaryWallet) {
+      return;
+    }
+
+    const signer = await getEthersSigner(primaryWallet.connector);
+    const signature = await signer.signMessage("hello world");
+    setSignature(signature)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={() => setShowAuthFlow(true)}>Connect wallet</button>
+      <div>
+        Primary wallet:
+        {primaryWallet && (
+          <>
+            <div>
+              <span>address: {primaryWallet.address} | </span>
+              <span>name: {primaryWallet.connector.name}</span>
+            </div>
+            <div>
+              <button onClick={signMessage}>sign message</button>
+              {signature && <div>signature: {signature}</div>}
+            </div>
+          </>
+        )}
+      </div>
+      <div>
+        Secondary wallets:
+        {secondaryWallets.map((wallet) => (
+          <div key={wallet.address}>
+            <span>address: {wallet.address} | </span>
+            <span>name: {wallet.connector.name} | </span>
+            <button onClick={() => setPrimaryWallet(wallet.id)}>
+              set primary
+            </button>
+            <button onClick={() => handleUnlinkWallet(wallet.id)}>
+              disconnect
+            </button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
 
-export default App;
+export default function WrappedPage() {
+  return (
+    <DynamicContextProvider
+      settings={{
+        environmentId: "373a7e50-1a9f-46dc-afd3-3777221bddb3",
+        initialAuthenticationMode: "connect-only",
+      }}
+    >
+      <Page />
+    </DynamicContextProvider>
+  );
+}
